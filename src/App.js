e061ab341fe62dd215b8cb49302e5ae22c1f68ae
Iines Piesala <audore@audore.local>
2019-11-16 09:50:41 +0200

    Add burger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Loader from "./components/Loader";
 import "./App.css";
+import Burger from './Burger'
 
 const App = () => {
   const [loading, setLoading] = React.useState(true);
@@ -10,7 +11,10 @@ const App = () => {
   if (loading) {
     return <Loader />;
   } else {
-    return <div className="app">blööblää</div>;
+    return <div className="app">
+      <Burger></Burger>
+      blööblää
+      </div>;
   }
 };
 

a37bdf48f15f317443f68e134e049d702f6f27f4
Iines Piesala <iinespiesala@gmail.com>
2019-11-16 12:09:46 +0200

    Use only free font awesome icons

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Burger.css';
 import { slide as Menu } from 'react-burger-menu'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHomeHeart, faSnooze,faAnalytics } from '@fortawesome/pro-solid-svg-icons'
+import { faHome, faBed} from '@fortawesome/free-solid-svg-icons'
  
 class Burger extends React.Component {
   showSettings (event) {
@@ -14,10 +14,10 @@ class Burger extends React.Component {
     return (
       <Menu left width={'20%'}>
 
-    <a id="today" className="menu-item" href="/"><FontAwesomeIcon icon={faHomeHeart} /></a>
-        <a id="sleep" className="menu-item" href="/about"><FontAwesomeIcon icon={faSnooze} /></a>
+    <a id="today" className="menu-item" href="/"><FontAwesomeIcon icon={faHome} /></a>
+        <a id="sleep" className="menu-item" href="/about"><FontAwesomeIcon icon={faBed} /></a>
         <a id="tips" className="menu-item" href="/contact">Tips</a>
-        <a id="activity" className="menu-item" href="/activity"><FontAwesomeIcon icon={faSnooze} /></a>
+        <a id="activity" className="menu-item" href="/activity"><FontAwesomeIcon icon={faBed} /></a>
         <a id="contact" className="menu-item" href="/activity">Contact</a>
       </Menu>
     );

f3e71c933ead6b58e1dd9851837ad14c168e9756
Minna Sarakontu <minna.sarakontu@aava.fi>
2019-11-16 19:39:03 +0200

    new burger items

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -6,25 +6,41 @@ import {
   faHome,
   faBed,
   faHandsHelping,
-  faFileAlt
+  faFileAlt,
+  faFlagCheckered,
+  faSmile,
+  faQuestion,
+  faAward,
+  faSchool
 } from "@fortawesome/free-solid-svg-icons";
 
 const Burger = ({ setView }) => (
   <Menu left width={"20%"}>
-    <div id="today" className="menu-item" onClick={() => setView(null)}>
+    <div className="menu-item" onClick={() => setView(null)}>
       <FontAwesomeIcon icon={faHome} /> Home
     </div>
-    <div id="sleep" className="menu-item" href="#">
+    <div className="menu-item" onClick={() => setView("mood")}>
+      <FontAwesomeIcon icon={faSmile} /> Mood
+    </div>
+    <div className="menu-item">
       <FontAwesomeIcon icon={faBed} /> Sleep
     </div>
-    <div
-      id="activity"
-      className="menu-item"
-      onClick={() => setView("fairdata")}
-    >
+    <div className="menu-item">
+      <FontAwesomeIcon icon={faSchool} /> School
+    </div>
+    <div className="menu-item">
+      <FontAwesomeIcon icon={faQuestion} /> Quizes
+    </div>
+    <div className="menu-item">
+      <FontAwesomeIcon icon={faFlagCheckered} /> Tasks
+    </div>
+    <div className="menu-item">
+      <FontAwesomeIcon icon={faAward} /> Bonuses
+    </div>
+    <div className="menu-item" onClick={() => setView("fairdata")}>
       <FontAwesomeIcon icon={faFileAlt} /> Fair Data
     </div>
-    <div id="activity" className="menu-item" onClick={() => setView("help")}>
+    <div className="menu-item" onClick={() => setView("help")}>
       <FontAwesomeIcon icon={faHandsHelping} /> Help
     </div>
   </Menu>
